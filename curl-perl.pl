8c84565f9459cb3e81e7c2dbc259caa23aa129a1
vdash <patrick.mccoy@aeris.net>
2013-01-15 15:02:36 -0800

    added time-date

--- curl-perl.pl
+++ curl-perl.pl
@@ -1,18 +1,31 @@
-use strict;
 use warnings;
 use WWW::Curl::Easy;
+use XML::CSV;
 
 my $curl = WWW::Curl::Easy->new;
         
 my $user = "monitor";
 my $pwd = "0psmon0";
 
+
+
+#!/usr/local/bin/perl
+@timeData = localtime(time);
+
+@months = qw(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec);
+@weekDays = qw(Sun Mon Tue Wed Thu Fri Sat Sun);
+($second, $minute, $hour, $dayOfMonth, $month, $yearOffset, $dayOfWeek, $dayOfYear, $daylightSavings) = localtime();
+$year = 1900 + $yearOffset;
+$theTime = "$hour:$minute:$second, $weekDays[$dayOfWeek] $months[$month] $dayOfMonth, $year";
+print $theTime;
+
 $curl->setopt(CURLOPT_HEADER,1);
 $curl->setopt(CURLOPT_URL, 'http://10.2.0.200/kpi/hw/exportall.php');
 $curl->setopt(CURLOPT_GET, 1);
 #$curl->setopt(CURLOPT_VERIFYHOST, 0);
 #$curl->setopt(CURLOPT_VERIFYPEER, 0);
-$curl->setopt(CURLOPT_USERPWD,"$user:$pwd" );
+#$curl->setopt(CURLOPT_USERPWD,"$user:$pwd" );
+$curl->setopt(CURLOPT_RETURNTRANSFER, 1);
 
         # A filehandle, reference to a scalar or reference to a typeglob can be used here.
 my $response_body;
@@ -31,3 +44,6 @@ if ($retcode == 0) {
                 # Error code, type of error, error message
     print("An error happened: $retcode ".$curl->strerror($retcode)." ".$curl->errbuf."\n");
 }
+
+my $csv_obj = XML::CSV-new();
+
